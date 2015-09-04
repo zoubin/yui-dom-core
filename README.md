a2d34748028b15d644caa2c9547c33e3a7320b37
zoubin <zoubin04@gmail.com>
2015-09-04 13:20:23 +0800

    dom-core

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # yui-dom-core
-A clone of yui-dom-core
+
+A commonjs wrapper for [yui-dom-core](http://yuilibrary.com/yui/docs/api/files/dom_js_dom-core.js.html).
+
+Here are the exports:
+
+* [byId](http://yuilibrary.com/yui/docs/api/classes/DOM.html#method_byId)
+* [elementByAxis](http://yuilibrary.com/yui/docs/api/classes/DOM.html#method_elementByAxis)
+* [contains](http://yuilibrary.com/yui/docs/api/classes/DOM.html#method_contains)
+* [inDoc](http://yuilibrary.com/yui/docs/api/classes/DOM.html#method_inDoc)
+
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 
 exports.byId = require('./lib/byId');
-exports.allById = require('./lib/allById');
-
-exports.getId = require('./lib/getId');
-exports.setId = require('./lib/setId');
-
-exports.ancestor = require('./lib/ancestor');
-exports.ancestors = require('./lib/ancestors');
-
 exports.elementByAxis = require('./lib/elementByAxis');
-
 exports.contains = require('./lib/contains');
 exports.inDoc = require('./lib/inDoc');
-
-exports.isWindow = require('./lib/isWindow');
-
-exports.siblings = require('./lib/siblings');
 
